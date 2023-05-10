de71fa57170938e3ad49a726e5897600dba98f12
Ömer Yılmaz <omogalatasaray@gmail.com>
2023-05-10 14:39:13 +0300

    Add project files.

---
+++ Yuz_Tanımlama/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Yuz_Tanımlama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btn_goruntu = new System.Windows.Forms.Button();
+            this.btn_Algıla = new System.Windows.Forms.Button();
+            this.btn_kamera = new System.Windows.Forms.Button();
+            this.pictureYuz = new System.Windows.Forms.PictureBox();
+            this.pictureKamera = new System.Windows.Forms.PictureBox();
+            this.maskedTC = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYuz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(702, 411);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(584, 411);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnkaydet.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnkaydet.Location = new System.Drawing.Point(601, 530);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(190, 27);
+            this.btnkaydet.TabIndex = 19;
+            this.btnkaydet.Text = "Yeni Kayıt Oluştur";
+            this.btnkaydet.UseVisualStyleBackColor = false;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnEkle.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(595, 114);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(190, 26);
+            this.btnEkle.TabIndex = 18;
+            this.btnEkle.Text = "Kişi Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btn_goruntu
+            // 
+            this.btn_goruntu.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_goruntu.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_goruntu.Location = new System.Drawing.Point(601, 368);
+            this.btn_goruntu.Name = "btn_goruntu";
+            this.btn_goruntu.Size = new System.Drawing.Size(190, 27);
+            this.btn_goruntu.TabIndex = 16;
+            this.btn_goruntu.Text = "Kişiyi Tanı";
+            this.btn_goruntu.UseVisualStyleBackColor = false;
+            this.btn_goruntu.Click += new System.EventHandler(this.btn_goruntu_Click);
+            // 
+            // btn_Algıla
+            // 
+            this.btn_Algıla.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_Algıla.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Algıla.Location = new System.Drawing.Point(595, 46);
+            this.btn_Algıla.Name = "btn_Algıla";
+            this.btn_Algıla.Size = new System.Drawing.Size(190, 26);
+            this.btn_Algıla.TabIndex = 14;
+            this.btn_Algıla.Text = "Yüzü Algıla";
+            this.btn_Algıla.UseVisualStyleBackColor = false;
+            this.btn_Algıla.Click += new System.EventHandler(this.btn_Algıla_Click);
+            // 
+            // btn_kamera
+            // 
+            this.btn_kamera.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_kamera.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kamera.Location = new System.Drawing.Point(595, 12);
+            this.btn_kamera.Name = "btn_kamera";
+            this.btn_kamera.Size = new System.Drawing.Size(190, 26);
+            this.btn_kamera.TabIndex = 13;
+            this.btn_kamera.Text = "Kamerayı Aç";
+            this.btn_kamera.UseVisualStyleBackColor = false;
+            this.btn_kamera.Click += new System.EventHandler(this.btn_kamera_Click);
+            // 
+            // pictureYuz
+            // 
+            this.pictureYuz.BackColor = System.Drawing.Color.Transparent;
+            this.pictureYuz.Location = new System.Drawing.Point(595, 157);
+            this.pictureYuz.Name = "pictureYuz";
+            this.pictureYuz.Size = new System.Drawing.Size(196, 196);
+            this.pictureYuz.TabIndex = 12;
+            this.pictureYuz.TabStop = false;
+            // 
+            // pictureKamera
+            // 
+            this.pictureKamera.BackColor = System.Drawing.Color.Transparent;
+            this.pictureKamera.Location = new System.Drawing.Point(12, 12);
+            this.pictureKamera.Name = "pictureKamera";
+            this.pictureKamera.Size = new System.Drawing.Size(545, 545);
+            this.pictureKamera.TabIndex = 11;
+            this.pictureKamera.TabStop = false;
+            // 
+            // maskedTC
+            // 
+            this.maskedTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTC.Location = new System.Drawing.Point(595, 80);
+            this.maskedTC.Mask = "00000000000";
+            this.maskedTC.Name = "maskedTC";
+            this.maskedTC.Size = new System.Drawing.Size(190, 26);
+            this.maskedTC.TabIndex = 22;
+            this.maskedTC.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(825, 583);
+            this.Controls.Add(this.maskedTC);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btn_goruntu);
+            this.Controls.Add(this.btn_Algıla);
+            this.Controls.Add(this.btn_kamera);
+            this.Controls.Add(this.pictureYuz);
+            this.Controls.Add(this.pictureKamera);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYuz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureKamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btn_goruntu;
+        private System.Windows.Forms.Button btn_Algıla;
+        private System.Windows.Forms.Button btn_kamera;
+        private System.Windows.Forms.PictureBox pictureYuz;
+        private System.Windows.Forms.PictureBox pictureKamera;
+        private System.Windows.Forms.MaskedTextBox maskedTC;
+    }
+}
+
---
+++ Yuz_Tanımlama/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Yuz_Tanımlama
+{
+    public partial class Form1 : Form
+    {
+        #region Degiskenler
+        int testid = 0;
+        private Capture yakala = null;
+        private Image<Bgr, Byte> mevcutyuz = null;
+        Mat cerceve = new Mat();
+        private bool yuzualgıla = false;
+        CascadeClassifier YuzCascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+        Image<Bgr, Byte> yuzsonuc = null;
+        List<Image<Gray, Byte>> denemeyuz = new List<Image<Gray, byte>>();
+        List<int> kisiler = new List<int>();
+
+        bool yuzkaydedici = false;
+        private bool durum = false;
+        EigenFaceRecognizer recognizer;
+        List<string> kisiad = new List<string>();
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_kamera_Click(object sender, EventArgs e)
+        {
+            //KAMERAYI AÇMA
+            if (yakala != null) yakala.Dispose();
+            yakala = new Capture();
+            Application.Idle += Yakala_Cerceve;
+        }
+
+        private void Yakala_Cerceve(object sender, EventArgs e)
+        {
+            //VIDEO YAKALAMA
+            if (yakala != null && yakala.Ptr != IntPtr.Zero)
+            {
+                yakala.Retrieve(cerceve, 0);
+                mevcutyuz = cerceve.ToImage<Bgr, Byte>().Resize(pictureKamera.Width, pictureKamera.Height, Inter.Cubic);
+
+                //YUZ TANIMA
+                if (yuzualgıla)
+                {
+
+                    //Arka Plandan Gri Görüntüye Dönüştür
+                    Mat Gribox = new Mat();
+                    CvInvoke.CvtColor(mevcutyuz, Gribox, ColorConversion.Bgr2Gray);
+                    //Daha iyi sonuç almak için görüntüyü geliştirin
+                    CvInvoke.EqualizeHist(Gribox, Gribox);
+
+                    Rectangle[] yuzler = YuzCascadeClassifier.DetectMultiScale(Gribox, 1.1, 3, Size.Empty, Size.Empty);
+                    //Yüzler algılanırsa
+                    if (yuzler.Length > 0)
+
+
+                        foreach (var yuz in yuzler)
+                        {
+                            // Her yüzün çevresine kare çizin
+                            CvInvoke.Rectangle(mevcutyuz, yuz, new Bgr(Color.Red).MCvScalar, 2);
+
+                            //KISI EKLEME
+                            //Yüzü pictureYuz'e atayın 
+                            Image<Bgr, Byte> resultImage = mevcutyuz.Convert<Bgr, Byte>();
+                            resultImage.ROI = yuz;
+                            pictureYuz.SizeMode = PictureBoxSizeMode.StretchImage;
+                            pictureYuz.Image = resultImage.Bitmap;
+
+                            if (yuzkaydedici)
+                            {
+                                //Mevcut değilse bir dizin oluşturacağız!
+                                string path = Directory.GetCurrentDirectory() + @"\YakalananResimler";
+                                if (!Directory.Exists(path))
+                                    Directory.CreateDirectory(path);
+                                //her görüntü için saniye gecikmeli 10 görüntü kaydedeceğiz 
+                                //askıda kalma GUI'sinden kaçınmak için yeni bir görev oluşturacağız
+                                Task.Factory.StartNew(() =>
+                                {
+                                    for (int i = 0; i < 10; i++)
+                                    {
+                                        //görüntüyü yeniden boyutlandırın ve kaydedin
+                                        resultImage.Resize(200, 200, Inter.Cubic).Save(path + @"\" + maskedTC.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".jpg");
+                                        Thread.Sleep(1000);
+                                    }
+                                });
+
+                            }
+                            yuzkaydedici = false;
+
+                            if (btnEkle.InvokeRequired)
+                            {
+                                btnEkle.Invoke(new ThreadStart(delegate
+                                {
+                                    btnEkle.Enabled = true;
+                                }));
+                            }
+
+                            //YUZU TANIMAK
+                            if (durum)
+                            {
+                                Image<Gray, Byte> grayFaceResult = resultImage.Convert<Gray, Byte>().Resize(100, 100, Inter.Cubic);
+                                CvInvoke.EqualizeHist(grayFaceResult, grayFaceResult);
+                                var result = recognizer.Predict(grayFaceResult);
+                                pictureBox1.Image = grayFaceResult.Bitmap;
+                                pictureBox2.Image = denemeyuz[result.Label].Bitmap;
+                                Debug.WriteLine(result.Label + ". " + result.Distance);
+                                //Sonuçlar bilinen yüzleri buldu
+                                if (result.Label != -1 && result.Distance < 2000)
+                                {
+                                    CvInvoke.PutText(mevcutyuz, kisiad[result.Label], new Point(yuz.X - 2, yuz.Y - 2),
+                                        FontFace.HersheyComplex, 1.0, new Bgr(Color.Blue).MCvScalar);
+                                    CvInvoke.Rectangle(mevcutyuz, yuz, new Bgr(Color.Green).MCvScalar, 2);
+                                }
+                                //burada sonuçlar bilinen herhangi bir yüz bulamadı
+                                else
+                                {
+                                    CvInvoke.PutText(mevcutyuz, "TANINAMADI", new Point(yuz.X - 2, yuz.Y - 2),
+                                        FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                                    CvInvoke.Rectangle(mevcutyuz, yuz, new Bgr(Color.Red).MCvScalar, 2);
+
+                                }
+                            }
+                        }
+                }
+            }
+            //Yakalanan videoyu Picture Box picKamera içine işleyin
+            pictureKamera.Image = mevcutyuz.Bitmap;
+
+        }
+
+        private void btn_Algıla_Click(object sender, EventArgs e)
+        {
+            yuzualgıla = true;
+        }
+
+        bool durum2;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(maskedTC.Text.Trim()))
+            {
+                durum2 = false;
+                yuzkaydedici = false;
+                MessageBox.Show("Hata: Lütfen Kişi TC Giriniz!");
+                return;
+            }
+            else
+            {
+                btnEkle.Enabled = false;
+                yuzkaydedici = true;
+                MessageBox.Show("3 Saniye Boyunca Kafanızı Sağ-Sol Yönünde Hareket Ettiriniz");
+                return;
+
+            }
+
+        }
+
+        private void btn_goruntu_Click(object sender, EventArgs e)
+        {
+            DizinGoruntuleri();
+        }
+        //Önceki örnekten kaydedilen görüntüleri kullanacağız
+        private bool DizinGoruntuleri()
+        {
+            int ImagesCount = 0;
+            double Threshold = 2000;
+            denemeyuz.Clear();
+            kisiler.Clear();
+            kisiad.Clear();
+            try
+            {
+                string path = Directory.GetCurrentDirectory() + @"\YakalananResimler";
+                string[] files = Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    Image<Gray, byte> trainedImage = new Image<Gray, byte>(file).Resize(100, 100, Inter.Cubic);
+                    CvInvoke.EqualizeHist(trainedImage, trainedImage);
+                    denemeyuz.Add(trainedImage);
+                    kisiler.Add(ImagesCount);
+                    string name = file.Split('\\').Last().Split('_')[0];
+                    kisiad.Add(name);
+                    ImagesCount++;
+                    Debug.WriteLine(ImagesCount + ". " + name);
+
+                }
+
+                if (denemeyuz.Count() > 0)
+                {
+                    
+                    recognizer = new EigenFaceRecognizer(ImagesCount, Threshold);
+                    recognizer.Train(denemeyuz.ToArray(), kisiler.ToArray());
+                    durum = true;
+                    return true;
+                }
+                else
+                {
+                    durum = false;
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                durum = false;
+                MessageBox.Show("Yüz İçin Hatalı Deneme " + ex.Message);
+                return false;
+            }
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            //uygulamayı daha hızlı reboot etmek için
+            Application.Exit();
+            // Uygulama hemen sonlandırılıyor.
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+            // İşletim sistemi tarafından uygulama tekrar başlatılıyor.
+        }
+    }
+}
